d2ef088cab5f4a3413b574f6444948dbd39b6319
alina <alina.mathyahs@gmail.com>
2021-06-17 18:17:37 +0300

    +

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Profile',
       home: MyHomePage(
-        title: 'Profile',
+        title: 'Profiler',
       ),
     );
   }
